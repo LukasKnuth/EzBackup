e6851907f8833fc831762cb446005a75c9e7504d
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-09-22 15:04:45 +0200

    Begin organizing bigger steps into operations.

---
+++ operations/backup.go
@@ -0,0 +1,18 @@
+package operations
+
+import (
+	"fmt"
+)
+
+func Backup() {
+	// todo safe_init
+	// todo backup
+	// todo retain
+	// todo purge
+	fmt.Println("TODO: run backup with Restic")
+}
+
+func Restore() {
+	// todo restore
+	fmt.Println("TODO: Run restore with Restic")
+}
---
+++ operations/k8s.go
@@ -0,0 +1,62 @@
+package operations
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/LukasKnuth/EzBackup/k8s"
+	"github.com/LukasKnuth/EzBackup/util"
+)
+
+func ScaleDown(pvcName string, options *k8s.RequestOptions, force bool, timeout time.Duration) (util.SpecificSet, error) {
+	fmt.Println("Looking for Pods mounting the PVC...")
+	filtered, err := k8s.FindMountingPods(pvcName, options)
+	if err != nil {
+		return nil, err
+	}
+
+	tree := util.MakeSet(len(filtered)) // could be smaller, but this is a good guess...
+
+	for _, pod := range filtered {
+		fmt.Printf("Mounted by %s: %s\n", "Pod", pod.Name)	
+		podDependencies, err := k8s.DependencyTree(&pod, options)
+		if err != nil {
+			if force {
+				fmt.Printf("WARN: %s -- Ignoring and continuing because --force was specified\n", err)
+			} else {
+				return nil, err
+			}
+		} else {
+			tree.PutAll(podDependencies)
+		}
+	}
+	if len(tree) > 0 {
+		fmt.Printf("Should scale %d resources\n", len(tree))
+		term_signal := k8s.AwaitTermination(filtered, options, timeout)
+		for _, res := range tree {
+			fmt.Printf("  %s: %s\n", res.Kind(), res.Name())
+			err := res.Surrender(options, force)
+			if err != nil { // todo better way?
+				return nil, err
+			}
+		}
+		if <-term_signal == k8s.Timeout {
+			fmt.Println("Timed out waiting for Pods to go down!")
+			// todo do we restore everything here?
+		}
+	}
+	fmt.Println("All dependencies shut down, continuing...")
+	return tree, nil
+}
+
+func ScaleUp(owners util.SpecificSet, options *k8s.RequestOptions) (err error) {
+	fmt.Printf("Scaling %d resources back up\n", len(owners))
+	for _, res := range owners {
+		fmt.Printf("  %s: %s\n", res.Kind(), res.Name())
+		err = res.Restore(options)
+		if err != nil {
+			return
+		}
+	}
+	return nil
+}
