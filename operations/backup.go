1669dc6de516cc39121ee6450cdc1de91c48e9c0
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-12-10 16:07:45 +0100

    Implemented basic backup operations.

--- operations/backup.go
+++ operations/backup.go
@@ -2,17 +2,29 @@ package operations
 
 import (
 	"fmt"
+	"errors"
+
+	"github.com/LukasKnuth/EzBackup/restic"
 )
 
-func Backup() {
-	// todo safe_init
-	// todo backup
-	// todo retain
-	// todo purge
-	fmt.Println("TODO: run backup with Restic")
+func Backup(pvcName string) error {
+	err := restic.SafeInit()
+	if err != nil {
+		return errors.New("Restic could not initialize repository. See output for details...")
+	}
+	err = restic.Backup(pvcName)
+	if err != nil {
+		return errors.New("Restic could not start/finish backup. See output for details...")
+	}
+	fmt.Println("TODO: run RETAIN via restic")
+	fmt.Println("TODO: run PURGE via restic")
+	return nil
 }
 
-func Restore() {
-	// todo restore
-	fmt.Println("TODO: Run restore with Restic")
+func Restore(pvcName string) error {
+	err := restic.Restore(pvcName)
+	if err != nil {
+		return errors.New("Restic could not start/complete restore. See output for details...")
+	}
+	return nil
 }
